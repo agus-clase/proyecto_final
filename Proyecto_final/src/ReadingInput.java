a117c42a1cef4379d6edc57ea8f18fab63ef863c
Agus <agus55510@gmail.com>
2022-05-27 11:30:06 +0200

    v2.0

---
+++ Proyecto_final/src/ReadingInput.java
@@ -0,0 +1,36 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import javax.swing.JTextArea;
+
+public class ReadingInput extends Thread {
+	
+	 protected BufferedReader input;
+	 protected JTextArea textArea;
+	 
+	 public ReadingInput (BufferedReader input, JTextArea text) {
+		 this.input=input;
+		 this.textArea=text;
+	 }
+		// overriding method run()
+		public void run() {
+			while(true) {
+				
+				 try {
+					textArea.append(input.readLine());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			 }
+		}
+		}
+	 
+	 
+	 
+	 
+
--- Proyecto_final/src/Ventana_Conexiones.java
+++ Proyecto_final/src/Ventana_Conexiones.java
@@ -1,13 +1,53 @@
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.BevelBorder;
+
+
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.awt.event.ActionEvent;
 
 public class Ventana_Conexiones {
 
 	private JFrame frmOrderManagingSoftware;
+	private final static int PORT = 5008;
+	private final static String SERVER = "localhost";
+	private ServerSocket server;
+	private Socket client;
+	private BufferedReader input;
+	private PrintStream output;
+	
+	// atributos ventana_oficina
+	private JFrame frm_oficina;
+	private JTextField textField_nombre;
+	private JTextField textField_id;
+	private JTextField textField_precio;
+	private JTextField textField_cantidad;
+	private JTextField textField_categoria;
+	private JButton btnEnviarPedido;
+	private JLabel lblNombre;
+	private JLabel lblCantidad;
+	private JLabel lblPrecio;
+	private JLabel lblCantidad_1;
+	private JLabel lblCategoria;
+	
+	//atributos ventana_almacen
+	private JFrame frm_almacen;
+	
 
 	/**
 	 * Launch the application.
@@ -36,6 +76,152 @@ public class Ventana_Conexiones {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		// ventana_oficina
+		
+				frm_oficina = new JFrame();
+				frm_oficina.setTitle("Stock Manager");
+				frm_oficina.setBounds(100, 100, 1280, 800);
+				// frm_oficina.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frm_oficina.getContentPane().setLayout(null);
+				
+				JPanel panel = new JPanel();
+				panel.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+				panel.setBounds(45, 38, 1173, 170);
+				frm_oficina.getContentPane().add(panel);
+				panel.setLayout(null);
+				
+				JLabel lblAadirProducto = new JLabel("Añadir pedido:");
+				lblAadirProducto.setFont(new Font("Dialog", Font.BOLD, 20));
+				lblAadirProducto.setBounds(503, 12, 197, 28);
+				panel.add(lblAadirProducto);
+				
+				textField_nombre = new JTextField();
+				textField_nombre.setFont(new Font("Dialog", Font.PLAIN, 15));
+				textField_nombre.setBounds(53, 99, 160, 28);
+				panel.add(textField_nombre);
+				textField_nombre.setColumns(10);
+				
+				textField_id = new JTextField();
+				textField_id.setFont(new Font("Dialog", Font.PLAIN, 15));
+				textField_id.setColumns(10);
+				textField_id.setBounds(243, 99, 160, 28);
+				panel.add(textField_id);
+				
+				textField_precio = new JTextField();
+				textField_precio.setFont(new Font("Dialog", Font.PLAIN, 15));
+				textField_precio.setColumns(10);
+				textField_precio.setBounds(432, 99, 160, 28);
+				panel.add(textField_precio);
+				
+				textField_cantidad = new JTextField();
+				textField_cantidad.setFont(new Font("Dialog", Font.PLAIN, 15));
+				textField_cantidad.setColumns(10);
+				textField_cantidad.setBounds(619, 99, 160, 28);
+				panel.add(textField_cantidad);
+				
+				textField_categoria = new JTextField();
+				textField_categoria.setFont(new Font("Dialog", Font.PLAIN, 15));
+				textField_categoria.setColumns(10);
+				textField_categoria.setBounds(814, 99, 160, 28);
+				panel.add(textField_categoria);
+				
+				btnEnviarPedido = new JButton("Enviar pedido");
+				btnEnviarPedido.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						JPanel panel1 = new JPanel();
+						try {
+						if (textField_nombre.getText().equals(null)||textField_id.getText().equals(null)||textField_precio.getText().equals(null)||
+								textField_cantidad.getText().equals(null)||textField_categoria.getText().equals(null)) {
+							
+							
+						}
+						else { 
+							Producto nuevo=new Producto(textField_nombre.getText(),Integer.valueOf(textField_id.getText()),
+								Double.valueOf(textField_precio.getText()),Integer.valueOf(textField_cantidad.getText()),
+								textField_categoria.getText());
+							
+							//para enviar el objeto al cliente
+							String prueba=nuevo.getNombre();
+							output.print(prueba);
+			//		textArea2.append(prueba);
+							
+							JOptionPane.showMessageDialog(
+									panel1, "Pedido enviado con éxito!", "Pedido enviado",
+								   	JOptionPane.PLAIN_MESSAGE);
+						}
+						}
+						catch(NumberFormatException ex){
+							JOptionPane.showMessageDialog(panel1,
+									"Completa todos los campos, por favor.", "Warning",
+									JOptionPane.WARNING_MESSAGE);
+							
+						}
+					}
+				});
+				btnEnviarPedido.setBounds(998, 99, 130, 27);
+				panel.add(btnEnviarPedido);
+				
+				lblNombre = new JLabel("Nombre:");
+				lblNombre.setFont(new Font("Dialog", Font.BOLD, 15));
+				lblNombre.setBounds(96, 57, 83, 17);
+				panel.add(lblNombre);
+				
+				lblCantidad = new JLabel("ID:");
+				lblCantidad.setFont(new Font("Dialog", Font.BOLD, 15));
+				lblCantidad.setBounds(302, 57, 83, 17);
+				panel.add(lblCantidad);
+				
+				lblPrecio = new JLabel("Precio:");
+				lblPrecio.setFont(new Font("Dialog", Font.BOLD, 15));
+				lblPrecio.setBounds(473, 57, 83, 17);
+				panel.add(lblPrecio);
+				
+				lblCantidad_1 = new JLabel("Cantidad:");
+				lblCantidad_1.setFont(new Font("Dialog", Font.BOLD, 15));
+				lblCantidad_1.setBounds(660, 57, 83, 17);
+				panel.add(lblCantidad_1);
+				
+				lblCategoria = new JLabel("Categoria:");
+				lblCategoria.setFont(new Font("Dialog", Font.BOLD, 15));
+				lblCategoria.setBounds(857, 57, 83, 17);
+				panel.add(lblCategoria);
+				
+				JTextArea textArea = new JTextArea();
+				textArea.setBounds(44, 300, 1174, 343);
+				frm_oficina.getContentPane().add(textArea);
+				
+				JButton btnNewButton = new JButton("Ver Stock");
+				btnNewButton.setBounds(583, 686, 105, 27);
+				frm_oficina.getContentPane().add(btnNewButton);
+				
+				//ventana_almacen
+				
+				frm_almacen = new JFrame();
+				frm_almacen.setBounds(100, 100, 600, 380);
+				// frm_almacen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frm_almacen.getContentPane().setLayout(null);
+				
+				JLabel lblInformacinDelPedido = new JLabel("Información del pedido:");
+				lblInformacinDelPedido.setFont(new Font("Dialog", Font.BOLD, 20));
+				lblInformacinDelPedido.setBounds(164, 27, 258, 28);
+				frm_almacen.getContentPane().add(lblInformacinDelPedido);
+				
+				JTextArea textArea2 = new JTextArea();
+				textArea2.setBounds(48, 76, 505, 153);
+				frm_almacen.getContentPane().add(textArea2);
+				
+				JButton btnNewButton2 = new JButton("Pedido Enviado");
+				btnNewButton.setBounds(48, 260, 142, 57);
+				frm_almacen.getContentPane().add(btnNewButton);
+				
+				JButton btnPedidoCancelado = new JButton("Pedido Cancelado");
+				btnPedidoCancelado.setBounds(411, 260, 142, 57);
+				frm_almacen.getContentPane().add(btnPedidoCancelado);
+		
+		//ventana conexiones
+		
+		
 		frmOrderManagingSoftware = new JFrame();
 		frmOrderManagingSoftware.setTitle("Order Managing Software v1.0");
 		frmOrderManagingSoftware.setBounds(100, 100, 600, 380);
@@ -46,6 +232,25 @@ public class Ventana_Conexiones {
 		btnConectar_oficina.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 	//			frmStockManager.setVisible(true);
+				try {
+					
+					frm_oficina.setVisible(true);
+					server = new ServerSocket(PORT);
+					client = server.accept();
+					// setSoLinger closes the socket giving 10mS to receive the remaining data
+					client.setSoLinger(true, 10);
+					input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+					// to print data out
+					output = new PrintStream(client.getOutputStream());
+
+					// INITIALIZE THREAD (TEXTAREA FROM ALMACEN MISSING)
+	//				ReadingInput t = new ReadingInput(input, textArea);
+	//				t.start();
+
+					
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
 			}
 		});
 		btnConectar_oficina.setBounds(116, 159, 141, 53);
@@ -54,9 +259,46 @@ public class Ventana_Conexiones {
 		JButton btnConectar_Almacen = new JButton("Conectar almacén");
 		btnConectar_Almacen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				try {
+					frm_almacen.setVisible(true);
+				client = new Socket(SERVER, PORT);// open socket
+				// To read from the server
+
+				input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				// to write to the server
+				output = new PrintStream(client.getOutputStream());
+				
+				ReadingInput t = new ReadingInput(input, textArea2);
+				t.start();
+				
+				String p=input.readLine();
+				textArea2.append(p);
+			}
+				catch (IOException ex) {
+					
+				}
 			}
 		});
 		btnConectar_Almacen.setBounds(351, 159, 141, 53);
 		frmOrderManagingSoftware.getContentPane().add(btnConectar_Almacen);
+		
+		JButton btnCerrar = new JButton("Cerrar conexión");
+		btnCerrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					server.close();
+					client.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		btnCerrar.setBounds(244, 248, 141, 53);
+		frmOrderManagingSoftware.getContentPane().add(btnCerrar);
+		
+		
 	}
-}
+	}
+
